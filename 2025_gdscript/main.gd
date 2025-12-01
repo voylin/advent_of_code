b40e3cb331b2d6026f924dd7cc1064814d2b809d
voylin <voylinslife@gmail.com>
2025-12-02 05:42:51 +0900

    Add timings

--- 2025_gdscript/main.gd
+++ 2025_gdscript/main.gd
@@ -19,5 +19,16 @@ func _ready() -> void:
 	current_day = load("res://days/%02d/script.gd" % day).new()
 
 	day_label.text =  "Day %02d" % day
+
+	# Part one + time measuring.
+	var now: int = Time.get_ticks_msec()
 	part_one_answer.text = str(current_day.part_one())
+	print_rich("[color=dim_gray][i]Part one finished: ", Time.get_ticks_msec() - now, " usec")
+
+	# Part two + time measuring.
+	now = Time.get_ticks_msec()
 	part_two_answer.text = str(current_day.part_two())
+	print_rich("[color=dim_gray][i]Part two finished: ", Time.get_ticks_msec() - now, " usec")
+
+	print("Answer part one: ", part_one_answer.text)
+	print("Answer part two: ", part_two_answer.text)
