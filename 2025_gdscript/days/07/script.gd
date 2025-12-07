40010317c10cbd05f3aa8420ce0532a9c303b9c1
voylin <voylinslife@gmail.com>
2025-12-07 14:53:41 +0900

    Day 07 - 2025

--- 2025_gdscript/days/07/script.gd
+++ 2025_gdscript/days/07/script.gd
@@ -1,11 +1,51 @@
 extends Day
 
+#var data_path: String = get_test_data_path()
+var data_path: String = get_data_path()
+
+var answers: PackedInt64Array = [0, 1]
+
 
 
 func part_one() -> int:
-	return 0
+	var data: PackedStringArray = Data.get_string_array(data_path)
+	var beams: Array = [data[0].find('S')]
+
+	for i: int in (data.size() / 2.0) - 1:
+		var line: String = data[2+i*2]
+		var new_beams: Array = []
+		
+		for beam: int in beams:
+			if line[beam] == '^':
+				answers[0] += 1
+				if !new_beams.has(beam-1): new_beams.append(beam-1)
+				if !new_beams.has(beam+1): new_beams.append(beam+1)
+			elif !new_beams.has(beam):
+				new_beams.append(beam)
+
+		beams = new_beams
+
+	return answers[0]
 
 
 func part_two() -> int:
-	return 0
+	var data: PackedStringArray = Data.get_string_array(data_path)
+	var beams: Dictionary = { data[0].find('S'): 1 }
 
+	for i: int in (data.size() / 2.0) - 1:
+		var line: String = data[2+i*2]
+		
+		for beam: int in beams:
+			if line[beam] == '^':
+				answers[1] += beams[beam]
+
+				if !beams.has(beam-1):
+					beams[beam-1] = 0
+				if !beams.has(beam+1):
+					beams[beam+1] = 0
+
+				beams[beam-1] += beams[beam]
+				beams[beam+1] += beams[beam]
+				beams[beam] = 0
+
+	return answers[1]
