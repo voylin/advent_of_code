16a60ae72e2e6ac3ce2666c7a02e0578cbcb05bd
voylin <voylinslife@gmail.com>
2025-12-11 21:40:19 +0900

    Day 10 - 2025 - Part 2 brute forcing
    Well, you need a lot of ram ... and time.

--- 2025_gdscript/days/10/script.gd
+++ 2025_gdscript/days/10/script.gd
@@ -54,7 +54,6 @@ func part_one() -> int:
 func part_two() -> int:
 	for machine: Machine in machines:
 		var value: int = machine.calculate_joltage()
-
 		print(value)
 		answers[1] += value
 		#answers[1] += machine.calculate_joltage()
@@ -66,12 +65,14 @@ class Machine:
 	var desired_state: PackedInt32Array = [] # 0000
 	var buttons: Array[PackedInt32Array] = []
 	var joltage: PackedInt32Array = []
+	var joltage_int: int = 0
 
 
 	func _init(lights: PackedInt32Array, b: Array[PackedInt32Array], j: PackedInt32Array) -> void:
 		desired_state = lights
 		buttons = b
 		joltage = j
+		joltage_int = _encode(j)
 
 
 	# use %2 = 0 is off and 1 is on	
@@ -116,41 +117,71 @@ class Machine:
 
 
 	func calculate_joltage() -> int:
-		var state: PackedInt32Array = []
-		var button_presses: Array[PackedInt32Array] = []
-		var history: Dictionary = { state: 0 }
+		var state: int = 0
+		var button_presses: PackedInt64Array = []
+		var history: Dictionary[int, int] = { state: 0 }
 		var turn: int = 0
 
-		state.resize(joltage.size())
-		state.fill(0)
 		button_presses.append(state)
 		history[state] = 0
 
 		while turn < button_presses.size():
-			var current_state: PackedInt32Array = button_presses[turn].duplicate()
-			var depth: int = history[current_state]
+			var depth: int = history[button_presses[turn]]
 
 			for button_group: PackedInt32Array in buttons:
-				var next_state: PackedInt32Array = current_state.duplicate()
+				var next_state: int = button_presses[turn]
 				var valid: bool = true
 
 				for i: int in button_group:
-					next_state[i] += 1
+					var shift: int = i * 8
+					var old_value: int = (next_state >> shift) & 0xFF
+					var new_value: int = old_value + 1
 
-					if next_state[i] > joltage[i]:
+					if new_value > joltage[i]:
 						valid = false
 						break
 
-				if not valid:
-					continue
-				elif next_state == joltage:
-					return depth + 1
+					# We remove the bits in the area of the 8 bits we updated
+					# ~() == bitwise not, so we set them all to opposite 1.
+					next_state &= ~(0xFF << shift)
+
+					# Now we do the bitwise OR to change all needed 0's by the
+					# 1's from the new value.
+					next_state |= new_value << shift
 
 				# New state so add to list
-				if not history.has(next_state):
+				if valid and not history.has(next_state):
+					if next_state == joltage_int:
+						return depth + 1
+
 					history[next_state] = depth + 1
 					button_presses.append(next_state)
+
+					if button_presses.size() >= 50001 and turn > 50000:
+						button_presses = button_presses.slice(50000)
+						turn -= 50000
 
 			turn += 1
 
 		return 0
+
+
+	func _encode(array: PackedInt32Array) -> int:
+		var out: int = 0
+
+		for i: int in array.size():
+			# Each joltage number should be under 255 so 8 bits is enough
+			out |= (array[i] & 0xFF) << (i * 8)
+
+		return out
+
+
+	func _decode(value: int) -> PackedInt32Array:
+		var array : PackedInt32Array = []
+
+		array.resize(joltage.size())
+
+		for i: int in joltage.size():
+			array[i] = (value >> (i * 8)) & 0xFF
+
+		return array
